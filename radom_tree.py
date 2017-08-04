2a7ecd7bfa625eedcdaade22989b6bc34fe46d71
qortmdgur5 <30610563+qortmdgur5@users.noreply.github.com>
2017-08-04 14:06:18 +0900

    v3

--- radom_tree.py
+++ radom_tree.py
@@ -1,21 +1,13 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jul 28 10:52:56 2017
-
-@author: USER
-"""
-
 #%%
 from skimage.draw import line
 import numpy as np
 from numpy.random import randint
 import matplotlib.pyplot as plt
 
-
 img = np.zeros((150,150), dtype=np.uint8)
-r = randint(50,60 , size=1)[0]
-c = randint(90,100 , size=1)[0]
-s = randint(70,80 , size=1)[0]
+r = randint(50,60 , size=1)
+c = randint(90,100 , size=1)
+s = randint(70,80 , size=1)
 
 #%%
 tree = line(100, 75, s, c)
@@ -27,26 +19,29 @@ img[tree2] = 1
 
 number = [1,1]
 for i in number:
-    a = randint(0,30 , size=1)[0]
-    b = randint(-10,30 , size=1)[0]
+    a = randint(5,30 , size=1)
+    b = randint(5,30 , size=1)
     tree3 = line(s, c, s-a*i, c+b*i)
-    d = randint(0,30 , size=1)[0]
-    e = randint(-10,30 , size=1)[0]
-    tree4 = line(s-a*i, c+b*i,  s-a*i-d, c+b*i+e)
-   
-    img[tree3] = 1
-    img[tree4] = 1
+    for k in range(2):
+        d = randint(5,30 , size=1)
+        e = randint(5,30 , size=1)
+        tree4 = line(s-a*i, c+b*i,  s-a*i-d, c+b*i+e)
+        img[tree3] = 1
+        img[tree4] = 1
     
 for i in number:
-    a = randint(0,30 , size=1)[0]
-    b = randint(-10,30 , size=1)[0]
+    a = randint(5,30 , size=1)
+    b = randint(5,30 , size=1)
     tree3 = line(s, r, s-a*i, r-b*i)
-    d = randint(0,30 , size=1)[0]
-    e = randint(-10,30 , size=1)[0]
-    tree4 = line(s-a*i, r-b*i,  s-a*i-d, r-b*i-e)
-    img[tree3] = 1
-    img[tree4] = 1
+    for k in range(2):     
+        d = randint(5,30 , size=1)
+        e = randint(5,30 , size=1)
+        tree4 = line(s-a*i, r-b*i,  s-a*i-d, r-b*i-e)
+        img[tree3] = 1
+        img[tree4] = 1
   
+   
+    
 
 plt.imshow(img)
 plt.show()
