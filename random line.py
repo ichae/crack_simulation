85c73a6522f268aee73073d9081181312632c958
qortmdgur5 <30610563+qortmdgur5@users.noreply.github.com>
2017-08-07 13:14:15 +0900

    Update random line.py

--- random line.py
+++ random line.py
@@ -12,22 +12,22 @@ import matplotlib.pyplot as plt
 
 img = np.zeros((500,500), dtype=np.uint8)
 
-    r = randint(0,499 , size=1)[0]
-    c = randint(0,499 , size=1)[0]
-    a = randint(0,499 , size=1)[0]
-    b = randint(0,499 , size=1)[0]
-    if c<b:
-        d = randint(c,b , size=1)[0]
-    else :
-        d = randint(b,c , size=1)[0]
-    if r<a:
-        e = randint(r,a , size=1)[0]
-    else :
-        e = randint(a,r , size=1)[0]
-    tree = line(r,c,e,d)
-    tree2 = line(e,d,a,b)
-    img[tree] = 1
-    img[tree2] = 1
+r = randint(0,499 , size=1)[0]
+c = randint(0,499 , size=1)[0]
+a = randint(0,499 , size=1)[0]
+b = randint(0,499 , size=1)[0]
+if c<b:
+    d = randint(c,b , size=1)[0]
+else :
+    d = randint(b,c , size=1)[0]
+if r<a:
+    e = randint(r,a , size=1)[0]
+else :
+    e = randint(a,r , size=1)[0]
+tree = line(r,c,e,d)
+tree2 = line(e,d,a,b)
+img[tree] = 1
+img[tree2] = 1
     
 
 
